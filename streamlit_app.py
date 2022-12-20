a8e8bb825d641462874b2777e9eb79d5e3dbe9cb
Rohan <120726427+rkdev2010@users.noreply.github.com>
2022-12-20 15:46:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,9 @@ streamlit.multiselect("Pick some fruits:", list(fruit_listed.index))
 streamlit.header('Fruityvice Fruit advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
